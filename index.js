0ec757ab9c2385b5017de9485898d63624cd01e5
Rishav Raj <rajrishav011@gmail.com>
2024-03-03 01:11:23 +0530

    Modify index.js file

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 
-const { Pool } = pg;
+
 const app = express();
 const port = process.env.PORT ||3000;
 
-const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL ,
-  user: process.env.POSTGRES_USER,
-  host:process.env.POSTGRES_HOST,
-  database: process.env.POSTGRES_DATABASE,
-  password: process.env.POSTGRES_PASSWORD, 
-})
-pool.connect();
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "123456",
+  port: 4000,
+});
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -23,7 +23,7 @@ let currentUserId = 1;
 let users = [];
 
 async function checkVisited() {
-  const result = await pool.query("SELECT country_code FROM visited_countries JOIN users ON users.id = user_id WHERE user_id = $1;",[currentUserId]);
+  const result = await db.query("SELECT country_code FROM visited_countries JOIN users ON users.id = user_id WHERE user_id = $1;",[currentUserId]);
   let countries = [];
   result.rows.forEach((country) => {
     countries.push(country.country_code);
@@ -31,7 +31,7 @@ async function checkVisited() {
   return countries;
 }
 async function getCurrentUser(){
-  const result = await pool.query("SELECT * FROM users");
+  const result = await db.query("SELECT * FROM users");
   users=result.rows;
   return users.find((user)=>user.id==currentUserId)
 }
@@ -49,14 +49,14 @@ app.post("/add", async (req, res) => {
   const input = req.body["country"];
 const currentUser = await getCurrentUser();
   try {
-    const result = await pool.query(
+    const result = await db.query(
       "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%';",
       [input.toLowerCase()]
     ); 
     const data = result.rows[0];
     const countryCode = data.country_code;
     try {
-      await pool.query(
+      await db.query(
         "INSERT INTO visited_countries (country_code , user_id) VALUES ($1 , $2);",
         [countryCode , currentUserId]
       );
@@ -65,20 +65,34 @@ const currentUser = await getCurrentUser();
       console.log(err);
       const countries = await checkVisited();
       res.render("index.ejs",{countries:countries,total:countries.length,users: users,
-        color: currentUser.color,error:'Country has already been added , try again.',})
-    
+      color: currentUser.color,error:'Country has already been added , try again.',})
     }
   } catch (err) {
     console.log(err);
+    res.render("index.ejs",{countries:countries,total:countries.length,users: users,
+    color: currentUser.color,error:'Country has already been added , try again.',})
   }
 });
 app.post("/user", async (req, res) => {
   if(req.body.add ==='new'){
     res.render('new.ejs');
+  }else if(currentUserId=req.body.remove){
+    try {
+      await db.query("Delete from visited_countries where user_id=$1;",[currentUserId]);
+   res.redirect("/");
+    } catch (error) {
+      console.log(error);
+    }
   }else{
     currentUserId=req.body.user;
-    res.redirect('/');
+    res.redirect("/"); 
   }
+  
+
+  /* 
+    res.render("index.ejs",{countries:countries,total:countries.length,users: users,
+      color: currentUser.color,error:'Country has already been added , try again.',})
+  } */
   });
 
 app.post("/new", async (req, res) => {
@@ -87,12 +101,15 @@ app.post("/new", async (req, res) => {
   const name = req.body.name;
   const color=req.body.color;
 
-  const result = await pool.query("INSERT INTO USERS (name , color) VALUES($1 , $2) RETURNING *;",[name , color]);
+  const result = await db.query("INSERT INTO USERS (name , color) VALUES($1 , $2) RETURNING *;",[name , color]);
   const id= result.rows[0].id;
   currentUserId = id;
   res.redirect('/');
 });
 
+
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
